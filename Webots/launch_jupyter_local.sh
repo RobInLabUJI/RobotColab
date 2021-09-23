e4e4ec56f20fbb1af48e8c005aa663e68bf2ea8b
usuario <usuario@labair03.act.uji.es>
2021-09-23 10:17:07 +0200

    configure scripts

---
+++ Webots/launch_jupyter_local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export WEBOTS_HOME=/usr/local/webots
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${WEBOTS_HOME}/lib/controller
+export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/controller/python38
+export PYTHONIOENCODING=UTF-8
+jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' \
+                 --port=8888 --NotebookApp.port_retries=0 --no-browser \
+                 --NotebookApp.token=''
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install jupyter_http_over_ws
+jupyter serverextension enable --py jupyter_http_over_ws
