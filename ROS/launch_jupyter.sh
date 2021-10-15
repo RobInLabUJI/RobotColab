73e5205dea4c63e2b95ff5b60a4ddef1ec138a50
Enric Cervera <ecervera@uji.es>
2021-10-15 12:39:17 +0200

    Allow external connections

--- ROS/launch_jupyter.sh
+++ ROS/launch_jupyter.sh
@@ -3,5 +3,5 @@
 source /opt/ros/noetic/setup.bash
 jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' \
                  --port=8888 --NotebookApp.port_retries=0 --no-browser \
-                 --NotebookApp.token=''
+                 --NotebookApp.token='' --ip 0.0.0.0
 
