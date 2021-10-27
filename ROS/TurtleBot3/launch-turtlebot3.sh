008c0041f512cc554417ff35e6abf9f5b8014317
Enric Cervera <ecervera@uji.es>
2021-10-27 07:35:15 +0000

    Launch ROS at boot

--- ROS/TurtleBot3/launch-turtlebot3.sh
+++ ROS/TurtleBot3/launch-turtlebot3.sh
@@ -1,1 +1,10 @@
-roslaunch turtlebot3_bringup turtlebot3_robot.launch
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
+
+export ROS_HOSTNAME=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
+export PATH=$PATH:/home/ubuntu/.local/bin
+
+roslaunch turtlebot3_bringup turtlebot3_robot.launch &
