b149fbe37279d842656ab221aba9a5b6c425d25a
Enric Cervera <35959353+cervera-enric@users.noreply.github.com>
2021-11-03 09:08:32 +0100

    Create ImageProcessing module

--- Webots/Pioneer3/Controllers.py
+++ Webots/Pioneer3/Controllers.py
@@ -4,18 +4,6 @@ import threading
 import time
 import math
 
-import IPython.display
-import PIL.Image
-
-def display(image, size=(300,200)):
-  """Display the image in the notebook."""
-  if image.dtype == np.dtype('uint8'):
-    PILimage = PIL.Image.fromarray(image)
-  elif image.dtype == np.dtype('float64'):
-    intimage = np.round(image/np.max(image)*255).astype('uint8')
-    PILimage = PIL.Image.fromarray(intimage)
-  IPython.display.display(PILimage.resize(size))
-
 __basicTimeStep__ = 10
 
 def step(ms=__basicTimeStep__):
