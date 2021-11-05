3d93b04538d24991c02ec025330146f7aaf9a6de
Enric Cervera <ecervera@uji.es>
2021-11-05 10:16:23 +0100

    Fix path

--- ROS/TurtleBot/launch-jupyter.sh
+++ ROS/TurtleBot/launch-jupyter.sh
@@ -4,7 +4,7 @@ source /opt/ros/noetic/setup.bash
 source /home/pi/catkin_ws/devel/setup.bash
 export PATH=$PATH:/home/pi/.local/bin
 
-cd /home/ubuntu/RobotColab/ROS && \
+cd /home/pi/RobotColab/ROS && \
 jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' \
                  --port=8888 --NotebookApp.port_retries=0 --no-browser \
                  --NotebookApp.token='' --ip 0.0.0.0 &
