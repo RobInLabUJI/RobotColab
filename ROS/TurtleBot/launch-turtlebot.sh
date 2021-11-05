2946c15f727b26cb784170729bb288e144e3180b
Enric Cervera <ecervera@uji.es>
2021-11-05 09:55:55 +0100

    Fix path

--- ROS/TurtleBot/launch-turtlebot.sh
+++ ROS/TurtleBot/launch-turtlebot.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
+
+export ROS_HOSTNAME=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
+export PATH=$PATH:/home/ubuntu/.local/bin
+
 roslaunch turtlebot_bringup minimal.launch
