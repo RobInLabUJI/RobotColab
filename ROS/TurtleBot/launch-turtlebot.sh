71a1e26edd29e51392305af96b46a69e141f1033
Enric Cervera <ecervera@uji.es>
2021-11-05 10:02:36 +0100

    Fix username

--- ROS/TurtleBot/launch-turtlebot.sh
+++ ROS/TurtleBot/launch-turtlebot.sh
@@ -5,6 +5,6 @@ source /home/ubuntu/catkin_ws/devel/setup.bash
 
 export ROS_HOSTNAME=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
 export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
-export PATH=$PATH:/home/ubuntu/.local/bin
+export PATH=$PATH:/home/pi/.local/bin
 
 roslaunch turtlebot_bringup minimal.launch
---
+++ ROS/TurtleBot/update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /home/pi/RobotColab && git pull
+
