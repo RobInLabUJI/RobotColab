ddef61a443d3c8f6126d0dc3822afbcfa9ce8257
Enric Cervera <ecervera@uji.es>
2021-11-18 08:57:34 +0000

    Add USB camera

---
+++ ROS/TurtleBot3/launch-usb-cam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
+
+export ROS_HOSTNAME=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
+export PATH=$PATH:/home/ubuntu/.local/bin
+
+roslaunch /home/ubuntu/RobotColab/ROS/TurtleBot3/usb_cam.launch
