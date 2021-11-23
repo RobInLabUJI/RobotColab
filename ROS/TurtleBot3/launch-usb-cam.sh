f3d373e3760b2020a4a1c83784e27dcf8f79fa29
Enric Cervera <ecervera@uji.es>
2021-11-23 09:51:57 +0000

    Launch in background

--- ROS/TurtleBot3/launch-usb-cam.sh
+++ ROS/TurtleBot3/launch-usb-cam.sh
@@ -7,4 +7,4 @@ export ROS_HOSTNAME=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/
 export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
 export PATH=$PATH:/home/ubuntu/.local/bin
 
-roslaunch /home/ubuntu/RobotColab/ROS/TurtleBot3/usb_cam.launch
+roslaunch /home/ubuntu/RobotColab/ROS/TurtleBot3/usb_cam.launch &
