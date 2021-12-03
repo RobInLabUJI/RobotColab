07681c562848d086bcb01b083d535a4d7917dde2
Enric Cervera <ecervera@uji.es>
2021-12-03 09:03:53 +0100

    Add gripper

--- ROS/Pioneer3/Controllers.py
+++ ROS/Pioneer3/Controllers.py
@@ -5,6 +5,8 @@ import nav_msgs.msg
 import sensor_msgs.msg
 from cv_bridge import CvBridge, CvBridgeError
 
+import p2os_msgs.msg
+
 def sleep(t):
   """Sleep t seconds in ROS time."""
   rospy.sleep(t)
@@ -15,6 +17,7 @@ class PioneerRobot:
   def __init__(self):
     rospy.init_node('controller', anonymous=True)
     self.pub = rospy.Publisher('cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
+    self.gripper = rospy.Publisher('gripper_control', p2os_msgs.msg.GripperState, queue_size=10)
     self.odomSub = rospy.Subscriber('pose', nav_msgs.msg.Odometry, self.odomCallback)
     self.bridge = CvBridge()
     self.imgSub = rospy.Subscriber('/camera/rgb/image_raw', sensor_msgs.msg.Image, self.imgCallback)
@@ -44,4 +47,44 @@ class PioneerRobot:
 
   def getImage(self):
     return self.cv_image
+
+  def gripperOpen(self):
+    gripperState = p2os_msgs.msg.GripperState()
+    grip = p2os_msgs.msg.GripState()
+    lift = p2os_msgs.msg.LiftState()
+    grip.state = 1
+    grip.dir = 0
+    gripperState.grip = grip
+    gripperState.lift = lift
+    self.gripper.publish(gripperState)
+
+  def gripperClose(self):
+    gripperState = p2os_msgs.msg.GripperState()
+    grip = p2os_msgs.msg.GripState()
+    lift = p2os_msgs.msg.LiftState()
+    grip.state = 2
+    grip.dir = 0
+    gripperState.grip = grip
+    gripperState.lift = lift
+    self.gripper.publish(gripperState)
+
+  def gripperUp(self):
+    gripperState = p2os_msgs.msg.GripperState()
+    grip = p2os_msgs.msg.GripState()
+    lift = p2os_msgs.msg.LiftState()
+    grip.state = 4
+    grip.dir = 0
+    gripperState.grip = grip
+    gripperState.lift = lift
+    self.gripper.publish(gripperState)
+
+  def gripperDown(self):
+    gripperState = p2os_msgs.msg.GripperState()
+    grip = p2os_msgs.msg.GripState()
+    lift = p2os_msgs.msg.LiftState()
+    grip.state = 5
+    grip.dir = 0
+    gripperState.grip = grip
+    gripperState.lift = lift
+    self.gripper.publish(gripperState)
 
