80b4a5bdb9875b994a3d4e2954f340b93fb5ae63
ecervera <Enric Cervera>
2022-09-21 10:44:32 +0200

    Motion, odometry, laser

--- ROS2/TurtleBot3/Controllers.py
+++ ROS2/TurtleBot3/Controllers.py
@@ -1,38 +1,73 @@
+import rclpy
+from rclpy.node import Node
+import nav_msgs.msg
+import sensor_msgs.msg
+import geometry_msgs.msg
+import math
+import numpy as np
 
-def sleep(t):
-  """Sleep t seconds in ROS time."""
-  #rospy.sleep(t)
-
-class TurtleBot3Robot:
-  """Proxy to TurtleBot 3 robot in ROS."""
+class TurtleBot3Robot(Node):
+  """Proxy to TurtleBot 3 robot in ROS 2."""
 
   def __init__(self):
-    pass
+    rclpy.init()
+    super().__init__('tb3')
+    
+    self.odomSubscription = self.create_subscription(
+        nav_msgs.msg.Odometry,
+        '/odom',
+        self.odomCallback,
+        10)
+    self.odomSubscription
+    
+    self.scanSubscription = self.create_subscription(
+        sensor_msgs.msg.LaserScan,
+        '/scan',
+        self.scanCallback,
+        10)
+    self.scanSubscription
+    a = np.linspace(0,360,360,False)*np.pi/180
+    self.c = np.cos(a)
+    self.s = np.sin(a)
+    
+    self.publisher_ = self.create_publisher(geometry_msgs.msg.Twist, '/cmd_vel', 10)
+    
+    rclpy.spin_once(self)
 
   def move(self, v, w):
-    pass
+    twist = geometry_msgs.msg.Twist()
+    twist.linear.x = float(v)
+    twist.angular.z = float(w)
+    self.publisher_.publish(twist)
+    rclpy.spin_once(self)
     
   def scanCallback(self, data):
-    pass
+    self.scan = data.ranges
 
   def odomCallback(self, data):
-    pass
+    self.x = data.pose.pose.position.x
+    self.y = data.pose.pose.position.y
+    self.w = data.pose.pose.orientation.w
+    self.z = data.pose.pose.orientation.z
 
   def imgCallback(self, data):
     pass
 
   def getPose(self):
+    rclpy.spin_once(self)
     halfTheta = math.atan2(self.z, self.w)
     return self.x, self.y, 2 * halfTheta
 
   def getScan(self):
-      return self.scan
+    rclpy.spin_once(self)
+    return self.scan
 
   def getPointCloud(self):
-      d = self.scan
-      x = np.multiply(d,self.c)
-      y = np.multiply(d,self.s)
-      return x, y
+    rclpy.spin_once(self)
+    d = self.scan
+    x = np.multiply(d, self.c)
+    y = np.multiply(d, self.s)
+    return x, y
 
   def getImage(self):
     return self.cv_image
