710b8fe92802b91b5653566f87783a1c087754ab
Enric Cervera <ecervera@uji.es>
2023-09-28 08:49:21 +0200

    Fix _xsrf error

--- Webots/launch_jupyter_local.sh
+++ Webots/launch_jupyter_local.sh
@@ -6,5 +6,6 @@ export PYTHONPATH=${PYTHONPATH}:${WEBOTS_HOME}/lib/controller/python38
 export PYTHONIOENCODING=UTF-8
 jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' \
                  --port=8888 --NotebookApp.port_retries=0 --no-browser \
-                 --NotebookApp.token=''
+                 --NotebookApp.token='' \
+                 --NotebookApp.disable_check_xsrf=True
 
