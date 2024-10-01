89dd347609b895707ebda56e0f3d3247f363022d
Enric Cervera <ecervera@uji.es>
2024-10-01 10:51:10 +0000

    Add local workspace for lds08

--- ROS2/TurtleBot3/launch-turtlebot3-lds-02.sh
+++ ROS2/TurtleBot3/launch-turtlebot3-lds-02.sh
@@ -2,6 +2,7 @@
 
 export ROS_DOMAIN_ID=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | cut -d . -f 4)
 source /opt/ros/humble/setup.bash
+source ~/ros2_ws/install/setup.bash
 
 export TURTLEBOT3_MODEL=burger
 export LDS_MODEL=LDS-02
