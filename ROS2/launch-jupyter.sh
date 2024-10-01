b0ebdb520a6a45c25507b1908c3f2f199b3c6f0d
Enric Cervera <ecervera@uji.es>
2024-10-01 10:09:34 +0000

    Use apt jupyter

--- ROS2/launch-jupyter.sh
+++ ROS2/launch-jupyter.sh
@@ -2,7 +2,7 @@
 
 export ROS_DOMAIN_ID=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | cut -d . -f 4)
 source /opt/ros/humble/setup.bash
-export PATH=$PATH:/home/ubuntu/.local/bin
+#export PATH=$PATH:/home/ubuntu/.local/bin
 
 cd /home/ubuntu/RobotColab/ROS2 && \
 jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' \
