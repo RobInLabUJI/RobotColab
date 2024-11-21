03348552fbe06d99c46febb7aa601808f6451071
Enric Cervera <ecervera@uji.es>
2024-11-21 14:46:22 +0100

    Check for image

--- ROS2/ImageProcessing/Algorithms.py
+++ ROS2/ImageProcessing/Algorithms.py
@@ -15,12 +15,15 @@ class Color(Enum):
 
 def display(image, size=(300,200)):
   """Display the image in the notebook."""
-  if image.dtype == np.dtype('uint8'):
-    PILimage = PIL.Image.fromarray(image)
-  elif image.dtype == np.dtype('float64'):
-    intimage = np.round(image/np.max(image)*255).astype('uint8')
-    PILimage = PIL.Image.fromarray(intimage)
-  IPython.display.display(PILimage.resize(size))
+  try:
+    if image.dtype == np.dtype('uint8'):
+      PILimage = PIL.Image.fromarray(image)
+    elif image.dtype == np.dtype('float64'):
+      intimage = np.round(image/np.max(image)*255).astype('uint8')
+      PILimage = PIL.Image.fromarray(intimage)
+    IPython.display.display(PILimage.resize(size))
+  except:
+    print("Cannot display image!")
 
 def colorFilter(image, color):
   LOW_S = 160
